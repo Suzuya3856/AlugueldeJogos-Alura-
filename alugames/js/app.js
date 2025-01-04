0c716a52a72af2a8798c6c574b95556bb69db076
Samuel <samuel.proenca31@gmail.com>
2025-01-04 14:33:43 -0300

    Projeto aluguel de jogos

---
+++ alugames/js/app.js
@@ -0,0 +1,17 @@
+function alterarStatus(id){
+    let jogo = document.getElementById(`game-${id}`)
+    let imagem = jogo.querySelector(".dashboard__item__img")
+    let botão = jogo.querySelector(".dashboard__item__button")
+
+    if (imagem.classList.contains("dashboard__item__img--rented")){
+        imagem.classList.remove("dashboard__item__img--rented")
+        botão.classList.remove("dashboard__item__button--return")
+        botão.textContent = "Alugar"
+    }
+
+    else {
+        imagem.classList.add("dashboard__item__img--rented")
+        botão.classList.add("dashboard__item__button--return")
+        botão.textContent = "Devolver"
+    }
+}
